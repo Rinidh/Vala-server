ec2437e125d4bd659ef16afabd72312b228c95b8
Rinidh <rinidhdhokia@gmail.com>
2024-04-21 16:54:49 +0300

    Add a safety check to validateObjectId middleware if id has been sent in body or params Write tests for validateObjectId when no id sent and when id sent as path params

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -1,7 +1,17 @@
 const { default: mongoose } = require("mongoose");
 
 function validateObjectId(req, res, next) {
-  const isValid = mongoose.Types.ObjectId.isValid(req.body._id);
+  const mongoId = req.body?._id //the mongo-id comes as _id field in mongo docs
+    ? req.body._id
+    : req.params?.id
+    ? req.params.id
+    : null;
+
+  if (!mongoId) {
+    res.status(401).send("No id provided in request body or path parameter...");
+  }
+
+  const isValid = mongoose.Types.ObjectId.isValid(mongoId);
   if (!isValid) {
     res.status(400).send("Invalid _id...");
   } else {
--- tests/unit-tests/middleware/validateObjectid.test.js
+++ tests/unit-tests/middleware/validateObjectid.test.js
@@ -2,12 +2,28 @@ const { default: mongoose } = require("mongoose");
 const validateObjectId = require("../../../middleware/validateObjectId");
 
 describe("validateObjectId", () => {
+  it("should return 401 if no id sent in either body or path param", async () => {
+    const req = {};
+    const res = {
+      status: jest.fn().mockReturnThis(), //or jest.fn(()=>res) to enable chaining ie res.status().send()
+      send: jest.fn().mockReturnThis(),
+    };
+    const next = jest.fn();
+
+    validateObjectId(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith(
+      "No id provided in request body or path parameter..."
+    );
+  });
+
   it("should send res status 400 if invalid mongo id", () => {
     const req = {
       body: { _id: "1234" },
     };
     const res = {
-      status: jest.fn().mockReturnThis(), //or jest.fn(()=>res) to enable chaining ie res.status().send()
+      status: jest.fn().mockReturnThis(),
       send: jest.fn().mockReturnThis(),
     };
     const next = jest.fn();
@@ -19,8 +35,9 @@ describe("validateObjectId", () => {
     expect(next).not.toHaveBeenCalled();
   });
 
-  it("should run next() if valid mongo id", () => {
+  it("should run next() if valid mongo id in body", () => {
     const req = {
+      param: {},
       body: { _id: new mongoose.Types.ObjectId().toHexString() },
     };
     const res = {};
@@ -30,5 +47,23 @@ describe("validateObjectId", () => {
     validateObjectId(req, res, next);
 
     expect(next).toHaveBeenCalled();
+  });
+
+  it("should run next() if valid mongo id in path paramater", () => {
+    const req = {
+      params: { id: new mongoose.Types.ObjectId().toHexString() },
+      body: {},
+    };
+    const res = {
+      status: jest.fn((code) => res), //retuning the same 'res' object due to chaining of meths ie res.status().send()
+      send: jest.fn((message) => res),
+    };
+    const next = jest.fn();
+    //avoid repetition of above and use mr mosh's tehnique
+
+    validateObjectId(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled(); //all .status() are only calls when there is an error
   });
 });
